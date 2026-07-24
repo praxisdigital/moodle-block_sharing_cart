c5fbb3690c971ec50386051ba399e7dbfa05c81b
Sam Møller <smo@moxis.dk>
2026-07-24 13:24:47 +0200

    praxisdigital/moodle-opgaver#1035: Fixed missing sharing cart icon
    Fix missing in format_onetopic when dragging course module to another course content drop area

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ GPL v3
 Change Log
 ----------
 
-* 5.2, release 2 2026.07.15
+* 5.2, release 2 2026.07.24
     * Fixed sharing cart icon doesn't appear after course module items due to JS only scanning the first element of course content.
 * 5.2, release 1 2026.06.11
     * Added support for Moodle 5.2
--- version.php
+++ version.php
@@ -6,7 +6,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->component = 'block_sharing_cart';
-$plugin->version = 2026071501;
+$plugin->version = 2026072401;
 $plugin->requires = 2026042000; // Moodle 5.2.0
 $plugin->release = '5.2, release 2';
 $plugin->maturity = MATURITY_STABLE;
