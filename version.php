844ced0fc0acf2fab39fbab33f88ad6d55cbd2b1
Anders Rasmussen <ara@praxis.dk>
2026-09-22 12:37:35 +0200

    https://github.com/praxisdigital/moodle-opgaver/issues/1105 - Added changes according to feedback

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->component = 'block_sharing_cart';
-$plugin->version = 2026091700;
+$plugin->version = 2026092000;
 $plugin->requires = 2023042400; // Moodle 4.2.0.
-$plugin->release = '5.1, release 3';
+$plugin->release = '5.1, release 2';
 $plugin->maturity = MATURITY_STABLE;
